32ce65b1888b2ca283b9d2c9cfba9425d023220c
mamoonatahir <mamoona.tahir3@gmail.com>
2021-01-26 21:42:56 +0500

    update

--- task1.js
+++ task1.js
@@ -1,13 +1,21 @@
 var a = prompt("What is your city name?");
 if(a == "karachi" || a == "Karachi" || a== "KARACHI") { 
     console.log("Welcome to the City of Lights");
-}
+    document.write("Welcome to the City of Lights");
+    }
 else if( a == "Lahore" || a == "lahore" || a == "LAHORE") {
     console.log("Welcome to the city of love");
+    document.write("Welcome to the city of love");
 }
 else if( a== "Quetta" || a == " QUETTA" || a == "quetta") {
-    console.log("welcome to the city of snow");
+    console.log("welcome to the city of snow")
+    document.write("welcome to the city of snow");
 }
 else if( a == "Pishawer" || a == "PISHAWER" || a == "pishawer") {
     console.log("Welcome to the city of kababs");
+    document.write("Welcome to the city of kababs");
+}
+else if ( a == "hyderabad" || a == "HYDERABAD" || a == "Hyderabad") {
+    console.log(" Welcome to the city of Rubri");
+    document.write(" Welcome to the city of Rubri");
 }
