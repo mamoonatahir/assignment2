e3fd22c8c440a652cf40924082272e3675f8f69b
mamoonatahir <mamoona.tahir3@gmail.com>
2021-01-26 21:58:11 +0500

    update

--- task1.js
+++ task1.js
@@ -1,21 +1,21 @@
 var a = prompt("What is your city name?");
 if(a == "karachi" || a == "Karachi" || a== "KARACHI") { 
     console.log("Welcome to the City of Lights");
-    document.write("Welcome to the City of Lights");
+    document.write("<h1>Welcome to the City of Lights</h1>");
     }
 else if( a == "Lahore" || a == "lahore" || a == "LAHORE") {
     console.log("Welcome to the city of love");
-    document.write("Welcome to the city of love");
+    document.write("<h1>Welcome to the city of love</h1>");
 }
 else if( a== "Quetta" || a == " QUETTA" || a == "quetta") {
     console.log("welcome to the city of snow")
-    document.write("welcome to the city of snow");
+    document.write("<h1>welcome to the city of snow</h1>");
 }
 else if( a == "Pishawer" || a == "PISHAWER" || a == "pishawer") {
     console.log("Welcome to the city of kababs");
-    document.write("Welcome to the city of kababs");
+    document.write("<h1>Welcome to the city of kababs</h1>");
 }
 else if ( a == "hyderabad" || a == "HYDERABAD" || a == "Hyderabad") {
     console.log(" Welcome to the city of Rubri");
-    document.write(" Welcome to the city of Rubri");
+    document.write(" <h1>Welcome to the city of Rubri</h1>");
 }
