b96d37d4092864af67bc9f197e3da8730126d165
mamoonatahir <mamoona.tahir3@gmail.com>
2021-01-26 22:28:43 +0500

    update

---
+++ task2.js
@@ -0,0 +1,30 @@
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+if (true){
+
+    alert("True");
+    }
+    if (false){
+    alert("False");
+    }
+    if("car" < "cat"){
+    
+    alert("car is smaller than cat");
+    }
