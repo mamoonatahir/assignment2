2171482c7a2dfc25593e2e49431d121a095892a4
mamoonatahir <mamoona.tahir3@gmail.com>
2021-02-05 02:54:54 +0500

    update

---
+++ task8.js
@@ -0,0 +1,11 @@
+var input1 = parseInt(prompt("enter the number"));
+
+if( input1 == 0){
+    console.log("this is zero");
+    }
+else if(input1 <=-1) {
+    console.log("this is negetive number" );
+}
+else if(input1 >=1){
+    console.log("this is positive number");
+}
