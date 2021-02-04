6c6c8f6ec54c220076e149d0a6af926723c502b2
mamoonatahir <mamoona.tahir3@gmail.com>
2021-02-04 12:15:47 +0500

    Update task6.js

--- task6.js
+++ task6.js
@@ -1,8 +1,10 @@
 var inputNum = parseInt(prompt("Enter the number"));
 var evenNum = inputNum % 2; 
 if(evenNum === 0) { 
- console.log(inputNum + " is an Even number ");
+ console.log( inputNum + " is an Even number ");
+ document.write( "<h1>Number "+ inputNum + " is an Even number</h1>");
 }
 else if(evenNum) {
     console.log(inputNum + " is an odd number");
+    document.write( "<h1>Number "+ inputNum + " is an Odd number</h1>");
 }
