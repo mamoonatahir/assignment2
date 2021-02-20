a754c5187925d3a1b82d5c481035481cfaa6f43b
mamoona tahir <mamoona.tahir3@gmail.com>
2021-02-20 22:47:36 +0500

    update

--- task5.js
+++ task5.js
@@ -1,7 +1,7 @@
 var a = +prompt("enter number");  
 b = a%2;
 if( b== 0 ){
-    console.log("this is a composit number")
+    console.log("This is a composit number")
 }
 else if( b <=1 ){
     console.log("This is prime number")
